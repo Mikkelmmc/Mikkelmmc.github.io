ac346edfbc9b7ad67b1ed707323d53633c9d18c4
Mikkelmmc <41455260+Mikkelmmc@users.noreply.github.com>
2018-12-23 14:31:14 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
+//Angularjs and jquery.datatable with ui.bootstrap and ui.utils
 
+var app=angular.module('formvalid', ['ui.bootstrap','ui.utils']);
+app.controller('validationCtrl',function($scope){
+  $scope.data=[
+        [
+            "401",
+            "Mikkel von Bobslæde",
+            "32",
+            "311BUT",
+            "80 44 84 47",
+            "Nej"
+        ],
+    ]
+
+$scope.dataTableOpt = {
+   //custom datatable options 
+  // or load data through ajax call also
+  "aLengthMenu": [[10, 50, 100,-1], [10, 50, 100,'All']],
+  };
+});
